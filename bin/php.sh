617cdc9c5475a0fe7c697ebe7a2739462b9849f4
duanhongyi <duanhongyi@doopai.com>
2021-07-05 16:48:40 +0800

    chore(pack-runtimes): change runtime to fpm

--- bin/php.sh
+++ bin/php.sh
@@ -5,7 +5,7 @@ PHP_BIN_PATH=${PHP_BIN_DIR}/php-${PHP_VERSION}-$(dpkg --print-architecture).tar.
 
 mkdir -p $PHP_TMP_DIR $PHP_BIN_DIR
 
-docker run -v $PHP_TMP_DIR:/tmp/php --rm -it docker.io/library/php:$PHP_VERSION-apache-buster bash -c "cp -rf /usr/local/* /tmp/php"
+docker run -v $PHP_TMP_DIR:/tmp/php --rm -it docker.io/library/php:$PHP_VERSION-fpm bash -c "cp -rf /usr/local/* /tmp/php"
 
 cd $PHP_TMP_DIR
 tar -zcvf $PHP_BIN_PATH .
